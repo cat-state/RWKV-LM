3591ded5306ff4ecac44174dbc9a8a503e0819bc
cat-state <cat-state@tutanota.com>
2022-08-23 02:35:25 +0100

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='rwkv_lm',
+      version='0.1',
+      description='RKWV-LM v4 + Image Package',
+      url='http://github.com/cat-state/RWKV-LM',
+      author='cat-state',
+      author_email='cat-state@tutanota.com',
+      license='BSD 2-Clause Licence',
+      packages=['rwkv_lm'],
+      zip_safe=False)
+
